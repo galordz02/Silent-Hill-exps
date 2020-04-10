928a8155ee86ff6367fdacfe40b2334909dff7e3
Hunson man <galordz.egura@gmail.com>
2020-04-10 02:13:18 -0500

    created two new files to experiment the image loading for the silentdex

---
+++ Silentdex/shcanvas.js
@@ -0,0 +1,38 @@
+var vp = document.getElementById("shcanvashtml");
+var paper = vp.getContext("2d");
+
+var background = {
+  url: "background.jpg",
+  loadOK: false
+}
+var valtiel = {
+  url: "valtiel.png",
+  loadOK: false
+};
+
+background.image = new Image();
+background.image.src = background.url;
+background.image.addEventListener("load", loadbackground);
+
+valtiel.image = new Image();
+valtiel.image.src = valtiel.url;
+valtiel.image.addEventListener("load", loadvaltiel);
+
+function loadbackground()
+{
+  background.loadOK = true;
+  draw();
+}
+function loadvaltiel()
+{
+  valtiel.loadOK = true;
+  draw();
+}
+
+function draw()
+{
+  if(background.loadOK)
+  {
+    paper.drawImage(background.image, 0, 0);
+  }
+}
