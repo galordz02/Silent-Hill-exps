0004a4516e459c59fb79c2b22ad84f27de3e5a7f
Hunson man <galordz.egura@gmail.com>
2020-04-10 06:46:57 -0500

    found a different way to invoke the show function

--- Silentdex/silent.js
+++ Silentdex/silent.js
@@ -1,17 +1,9 @@
 var images = [];
-images[1] = "Pyramid_Head.png";
-images[2] = "Abstract_Daddy.png";
+images[0] = "Pyramid_Head.png";
+images[1] = "Abstract_Daddy.png";
 
 var collection = [];
-collection.push(new Silentmon("Pyramid Head", 1, 600, 120));
-collection.push(new Silentmon("Abstract Daddy", 2, 600, 120));
+collection.push(new Silentmon("Pyramid Head", 0, 600, 120));
+collection.push(new Silentmon("Abstract Daddy", 1, 600, 120));
 
-for(var freddito of collection)
-{
-  freddito.show();
-}
-
-for(var x in collection[0])
-{
-  console.log(x);
-}
+collection[0].show();
