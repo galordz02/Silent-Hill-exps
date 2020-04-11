8c7ce03eecf1c6db2fa0941a37380db91d2dbd29
galordz <galordz.egura@gmail.com>
2020-04-11 02:32:14 -0500

    moved all the script of silentdex.hml to one single file because for some reason if its in different files, it wont work

--- Silentdex/silentmonster.js
+++ Silentdex/silentmonster.js
@@ -18,3 +18,16 @@ draw()
       paper.drawImage(this.image,this.xposition, this.yposition);
     }
 }
+
+
+
+var images = [];
+images[0] = "Pyramid_Head.png";
+images[1] = "Abstract_Daddy.png";
+
+var collection = [];
+collection.push(new Silentmon("Pyramid Head", 0, 600, 120));
+collection.push(new Silentmon("Abstract Daddy", 1, 600, 120));
+
+collection[0].draw();
+collection[0].image.addEventListener("draw()", alert("smn"));
