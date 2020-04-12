19258c577ad277e4fb5c31fcfcfb65873780ddc7
galordz <galordz.egura@gmail.com>
2020-04-11 22:38:23 -0500

    created silent.js. It was a mistake

---
+++ Silentdex/silent.js
@@ -0,0 +1,9 @@
+var images = [];
+images[1] = "Pyramid_Head.png";
+images[2] = "Abstract_Daddy.png";
+
+var collection = [];
+collection.push(new Silentmon("Pyramid Head", 1, 600, 120));
+collection.push(new Silentmon("Abstract Daddy", 2, 600, 120));
+
+collection[0].show();
