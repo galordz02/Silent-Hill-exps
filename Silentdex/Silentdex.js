2766aba484c3e339b1e0167ceda6bd6495c96070
galordz <galordz.egura@gmail.com>
2020-04-13 23:26:23 -0500

    just added another monster to the code and changed its size

--- Silentdex/Silentdex.js
+++ Silentdex/Silentdex.js
@@ -1,8 +1,11 @@
 var vp = document.getElementById("Silentdexhtml");
 var paper = vp.getContext("2d");
+
 var images = [];
 images[0] = "Pyramid_Head.png";
 images[1] = "Abstract_Daddy.png";
+images[2] = "Lustful_Lips.png";
+
 var images2 =[];
 images2[0] = "background.jpg"
 images2[1] = "pointer.png"
@@ -64,3 +67,4 @@ collection2.push(new OtherImage(2, 180, 320));
 var collection = [];
 collection.push(new Silentmon("Pyramid Head", 0, 600, 120));
 collection.push(new Silentmon("Abstract Daddy", 1, 600, 300));
+collection.push(new Silentmon("Lustful Lips", 2, 600, 120));
